c0df5ab4fa07963f55888ab0a2a716fe5876411a
大和祐介 <yamato@yamatoyuusukenoMacBook-Air.local>
2024-12-08 16:28:14 +0900

    軽微な変更

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
+import os
 from src.emograph import Builder
 
 
-def main(yaml_file: str, output_image: str):
+def main(yaml_file: str, output_image_name: str, output_dir = "./output"):
     builder = Builder()
 
     yaml_data = builder.load_yaml(yaml_file)
-    builder.generate_image(yaml_data, output_image)
+    output_path = os.path.join(output_dir, output_image_name)
+    builder.generate_image(yaml_data, output_path)
 
 
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description="YAML定義から画像を生成")
-    parser.add_argument('-Y', '--yaml', required=True, help="入力YAMLファイル")
-    parser.add_argument('-O', '--output', required=True, help="出力画像ファイル（例: output.png）")
+    parser.add_argument(
+        '--config',
+        required=True,
+        help="configを記述したyamlファイル",
+    )
+    parser.add_argument(
+        '--output_name',
+        required=False,
+        help="出力画像ファイル（例: output.png）",
+    )
     args = parser.parse_args()
 
-    main(yaml_file=args.yaml, output_image=args.output)
+    main(
+        yaml_file=args.config,
+        output_image_name=args.output_name
+    )
