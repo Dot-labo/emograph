85cb9452493934842ac692d957677e17baf70a53
大和祐介 <yamato@yamatoyuusukenoMacBook-Air.local>
2025-01-11 18:29:10 +0900

    srcにfontを追加

---
+++ src/emograph/fonts/__init__.py
@@ -0,0 +1,10 @@
+import os
+
+DEFAULT_FONT_PATH = os.path.join(os.path.dirname(__file__), "JKG-M_3.ttf")
+DEFAULT_EMOJI_FONT_PATH = os.path.join(os.path.dirname(__file__), "seguiemj.ttf")
+
+
+__all__ = [
+    "DEFAULT_FONT_PATH",
+    "DEFAULT_EMOJI_FONT_PATH",
+]
